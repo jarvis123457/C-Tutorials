196fca8288bbc3bacb51c2b49592360ec57f6c00
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-04-05 16:29:55 +0500

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    /* %d is used for integers, %f for float or real numbers, and %c for characters. */
+    // int a = 8.5; Not Recommended b/c 8.5 is not an integer.
+    int a = 4;
+    float b = 8.5;
+    char c = 'u';
+    printf("The value of a is %d, the value b is %f, and the value of c is %c", a, b, c);
+    
+    
+    return 0;
+}
+57:01
