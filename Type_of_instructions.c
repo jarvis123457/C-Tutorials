54a782025d413dd4cf0b99b4761a8255ed48f908
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-04-09 13:43:32 +0500

    Create Type_of_instructions.c

---
+++ Type_of_instructions.c
@@ -0,0 +1,37 @@
+/* Type of Instruction:
+Type Declaration Instruction:- int a, float b, declaring type of variables.
+Arithmetic Instruction:- Arithmetic operations
+Control Instruction:- for loops, while loops, decides which instruction runs first and order of program.*/
+#include <stdio.h>
+int main(){
+    int a = 4, b, c; // Type declaration instruction
+    printf("The value of a is %d\n", a); // \n is an escape sequence character.
+    return 0;
+    
+}
+// ARITHMETIC OPERATORS
+#include <stdio.h>
+int main(){
+    int a = 4;
+    int b = 8;
+    int z; 
+    z= b*a;/* This is a legal assignment of value to z. But
+    b*a=z is illegal because 4*8=32=z because this assigns 32 a value of z.*/
+    printf("The value of z is: %d\n", z);
+    printf("The value of a+b is: %d\n", a+b);/* a and b are OPERANDS and + - * / are ARITHMETIC OPERATORS */
+    printf("The value of a-b is: %d\n", a-b);
+    printf("The value of a*b is: %d\n", a*b);
+    printf("The value of a/b is: %d\n", a/b);
+    /* % is a modular division operator, returns the remainder, but cannot be applied on FLOAT, sign same as numerator. */
+    printf("5 when divided by 3 leaves a remainder of %d\n", -5%3);
+    printf("5 when divided by 3 leaves a remainder of %d\n", 5%-3);
+    return 0;
+    /* No operator to perform exponentiation in C. We need to
+    include a math library to do that. and use pow(a,b) which
+    returns a double hence always use %f for pow function. 
+    TYPE CONVERSION
+    int and int variables give int answer regardless of mathematical operation.
+    int and float variables give float answer regardless of mathematical operation.
+    float and float variables give float answer regardless of mathematical operation.*/
+    
+}
