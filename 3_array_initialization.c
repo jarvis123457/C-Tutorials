320f7002fd748f2b96bec6915e997d92c54e7e71
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-04-26 12:52:54 +0500

    Create 3_array_initialization.c

---
+++ 3_array_initialization.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a[]={34,232,23};
+  printf("The value of a[0] is %d\n", a[0]);
+  printf("The value of a[1] is %d\n", a[1]);
+  printf("The value of a[2] is %d\n", a[2]);
+  float b[]={6,69,86};
+  printf("The value of b[0] is %f\n", b[0]);
+  printf("The value of b[1] is %f\n", b[1]);
+  printf("The value of b[2] is %f\n", b[2]);
+  return 0;
+}
