907b6bd9f36cdd44abe2e09d107fabb8f62a6446
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-04-26 12:33:57 +0500

    Create 1_arrays_basic.c

---
+++ 1_arrays_basic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int marks[4]; // An array of 4 integer elements
+  marks[0] = 34;// First Element
+  marks[1] = 64;// Second Element
+  marks[2] = 94;// Third Element
+  marks[3] = 84;// Fourth Element
+  // Array exists as continuous blocks in memory.
+  return 0;
+}
