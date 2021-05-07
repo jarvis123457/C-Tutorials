31c3c19534e00d9205711b1823124aa73e7d7d00
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-05-08 01:40:16 +0500

    Update 3_inputing_Strings.c

--- 3_inputing_Strings.c
+++ 3_inputing_Strings.c
@@ -2,7 +2,7 @@
 int main(){
   char s[34];
   printf("Enter your name: \n");
-  scanf("%s", s);
+  scanf("%s", s);// String by this method does not allow multi-word or spaces between characters
   printf("Your name is: %s", s);
   return 0;
 }
