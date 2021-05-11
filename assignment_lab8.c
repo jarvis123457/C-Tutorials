8e8404a383c880163abe090ccd0b73e881a41ecd
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-05-11 18:59:22 +0500

    Update assignment_lab8.c

--- assignment_lab8.c
+++ assignment_lab8.c
@@ -47,3 +47,33 @@ void OddEven(int n){
   }
 }
 /*---------------------------------------------------------------------------------------*/
+/* Task 3: Write a program in C to convert decimal number to binary number using the function. */
+#include <stdio.h>
+#include <math.h>
+int DecimaltoBinary(int n, int a);
+int main(){
+  int n;
+  int a=0;
+  int b;
+  printf("Enter the value of decimal number: \n");
+  scanf("%d", &n);
+  b = DecimaltoBinary(n,a);
+  printf("The binary value of decimal number %d is: %d\n", n, b);
+  return 0;
+}
+int DecimaltoBinary(int n, int a){
+  int d=0;
+  int i;
+  for(i=0;d!=1;i++){
+    d=n/(pow(2, i));
+  }
+  a = a + pow(10, i-1);
+  if((n-pow(2,i-1))>0){
+    a = DecimaltoBinary(n-pow(2,i-1),a);
+    return a;
+    }
+  else{
+    return a;
+  }
+}
+/*---------------------------------------------------------------------------------*/
