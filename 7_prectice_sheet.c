b3831c2e06d837ceeabc5978fae1c80a1374f54d
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-05-17 10:33:58 +0500

    Create 7_prectice_sheet.c

---
+++ 7_prectice_sheet.c
@@ -0,0 +1,7 @@
+/* Q1: Which of the following is used to appropriately read a multi-word string:
+(a) gets()
+(b) puts()
+(c) printf
+(d) scanf
+Answer: gets() */
+/*------------------------------------------------------------------------------------*/
