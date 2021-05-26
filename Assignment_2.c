5c13b18bf7f5148b39602acccb40233be869d062
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-05-26 15:19:03 +0500

    Update Assignment_2.c

--- Assignment_2.c
+++ Assignment_2.c
@@ -20,3 +20,49 @@ void MultiplicationTable(int n, int a){
   }
 }
 /*-------------------------------------------------------------------------------------------*/
+/* Write a program which will have three functions. One function will take input of employee total salary, no. of leaves and extra sale amount. Next function will receive all information of employee and will deduct 1000 for one leave (no deduction for first two leaves) and add 50% 0f extra sale in final salary of employee. Third function will receive and display total salary of employee.
+Salary should be checked whether it is between 30000 and 70000. */
+#include <stdio.h>
+int EmployeeInfo();
+int SalCalculation(int salary,int leaves,int ExtSaleAmnt);
+void DisplaySalary(int a);
+int main(){
+  int a;
+  a = EmployeeInfo();
+  if(a<=0){
+    printf("Please Try Again!! Your Amount of Base Salary is Invalid\n");
+  }
+  else{
+    DisplaySalary(a);
+  }
+  return 0;
+}
+int EmployeeInfo(){
+  int salary;
+  int leaves;
+  int ExtSaleAmnt;
+  int a=0;
+  printf("Enter your monthly base salary\n");
+  scanf("%d",&salary);
+  if(salary>=30000 && salary<=70000){
+    printf("Enter the number of your leaves\n");
+    scanf("%d",&leaves);
+    printf("Enter the amount earned in Extra Sales\n");
+    scanf("%d",&ExtSaleAmnt);
+    a = SalCalculation(salary, leaves, ExtSaleAmnt);
+  }
+  else{
+    printf("Please Enter an amount between 30000 and 70000\n");
+  }
+  return a;
+}
+int SalCalculation(int salary,int leaves,int ExtSaleAmnt){
+  if(leaves>2){
+    salary -= (leaves-2)*1000;
+  }
+  salary += 0.5*ExtSaleAmnt;
+  return salary; 
+}
+void DisplaySalary(int a){
+  printf("The Amount of Your Final Salary is : %d\n",a);
+}
