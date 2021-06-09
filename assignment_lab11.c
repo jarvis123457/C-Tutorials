2384f944e53b344bedf67d6e8d93c31d36e0006d
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-06-09 19:14:08 +0500

    Update assignment_lab11.c

--- assignment_lab11.c
+++ assignment_lab11.c
@@ -21,3 +21,31 @@ int main(){
   }
 }
 /*-------------------------------------------------------------------------------------*/
+/* Write a program in C to multiply (2 matrix) 2d array, use your mathematics concepts for implementing the matrices multiplication formula. */
+#include <stdio.h>
+int main(){
+  int a[3][3];
+  int b[3][3];
+  int c[3][3];
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      printf("Enter the value of Element of Row %d and Column %d in array A.\n",i+1,j+1);
+      scanf("%d",&a[i][j]);
+      printf("Enter the value of Element of Row %d and Column %d in array B.\n",i+1,j+1);
+      scanf("%d",&b[i][j]);
+    }
+  }
+  printf("The Multplication of array B from array A:  \n");
+  for(int i=0;i<3;i++){
+    printf("[");
+    for(int j=0;j<3;j++){
+      c[i][j]=0;
+      for(int k=0;k<3;k++){
+        c[i][j]+=a[i][k]*b[k][j];
+      }
+      printf("%3d",c[i][j]);
+    }
+    printf(" ]");
+    printf("\n");
+  }
+}
