f418c52072099a3f870fb7f06f6505e4fb02dde7
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-06-22 21:23:08 +0500

    Update 7_prectice_sheet.c

--- 7_prectice_sheet.c
+++ 7_prectice_sheet.c
@@ -5,3 +5,28 @@
 (d) scanf
 Answer: gets() */
 /*------------------------------------------------------------------------------------*/
+/* Question 2 : Write a Program to take a string as input from the user using %c and %s. Confirm that strings are equal */
+#include <stdio.h>
+#include <string.h>
+int main(){
+  char st1[34];
+  char st2[34];
+  char c;
+  int i = 0;
+  printf("Enter the value of first string\n");
+  scanf("%s", st1);
+  printf("Enter the value of first string character by character\n");
+  fflush(stdin);
+  while(c!='\n'){
+    fflush(stdin);
+    scanf("%c", &c);
+    st2[i] = c;
+    i++;
+  }
+  st2[i-1] = '\0';
+  printf("The value of st1 is %s\n", st1);
+  printf("The value of st2 is %s\n", st2);
+  printf("strcmp for these values return %d\n", strcmp(st1,st2));
+  return 0;
+}
+/*-----------------------------------------------------------------------------------------------------*/
