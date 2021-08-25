e5a27995f5e9f7bf959dbf6d38ad9a89686719ad
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-08-25 19:19:51 +0500

    Create 4_passing_structure_to_function.c

---
+++ 4_passing_structure_to_function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+struct employee{
+  int code;
+  float salary;
+  char name[10];
+};
+void show(struct employee e);
+int main(){
+  struct employee e1;
+  struct employee *ptr;
+  ptr = &e1;
+  (*ptr).code=101;// short hand for this is on next line
+  ptr->code=105; // -> Arrow Operator
+  // printf("%d", e1.code);
+  ptr->salary=11.50;
+  strcpy(e1.name,"Lavish");
+  show(e1);
+  return 0; 
+}
+void show(struct employee e){
+  printf("The code of employee is : %d\n", e.code);
+  printf("The salary of employee is : %f\n", e.salary);
+  printf("The name of employee is : %s\n", e.name);
+}
