d8b4d32624813ebc847d40c2e6c4244e6959bcdb
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-08-25 19:23:27 +0500

    Update 4_passing_structure_to_function.c

--- 4_passing_structure_to_function.c
+++ 4_passing_structure_to_function.c
@@ -16,10 +16,12 @@ int main(){
   ptr->salary=11.50;
   strcpy(e1.name,"Lavish");
   show(e1);
+  printf("%d",e1.code);
   return 0; 
 }
 void show(struct employee e){
   printf("The code of employee is : %d\n", e.code);
   printf("The salary of employee is : %f\n", e.salary);
   printf("The name of employee is : %s\n", e.name);
+  e.code=546;//This doesnt change the value of the e1 in main function.
 }
