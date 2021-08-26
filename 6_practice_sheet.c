8c6efd4543f60f3944f81464b26f441e70d73b3e
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-08-26 15:46:33 +0500

    Update 6_practice_sheet.c

--- 6_practice_sheet.c
+++ 6_practice_sheet.c
@@ -14,3 +14,31 @@ int main(){
   printf("X dim is %d and Y dim is %d\n", v1.x, v1.y);  
   return 0;
 }
+/*--------------------------------------------------------------------------*/
+/* Question 2 => Write a function sumvector which returns the sum of two vectors passed to it. The vectors must be two-dimensional.        */
+#include<stdio.h>
+
+struct vector{
+  int x;
+  int y;
+};
+struct vector sumVector(struct vector v1, struct vector v2);
+
+
+int main(){
+  struct vector v1, v2, sum;
+  v1.x = 34;
+  v1.y = 54;
+  printf("X dim is %d and Y dim is %d\n", v1.x, v1.y);
+  v2.x=5;
+  v2.y=4;
+  sum = sumVector(v1,v2);
+  printf("X dim is %d and Y dim is %d\n", sum.x, sum.y);
+  return 0;
+}
+struct vector sumVector(struct vector v1, struct vector v2){
+  struct vector result;
+  result.x=v1.x+v2.x;
+  result.y=v1.y+v2.y;
+  return result;
+}
