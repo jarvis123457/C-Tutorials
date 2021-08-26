9b59f59cd114e51a613f77ef13d9003669b63af7
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-08-26 15:05:31 +0500

    Update 6_practice_sheet.c

--- 6_practice_sheet.c
+++ 6_practice_sheet.c
@@ -1,1 +1,16 @@
+/*                         Practice Problems                                  */
+/* Question 1 => Create a two dimensional vector using structures in C        */
+#include<stdio.h>
 
+struct vector{
+  int x;
+  int y;
+};
+
+int main(){
+  struct vector v1, v2;
+  v1.x = 34;
+  v1.y = 54;
+  printf("X dim is %d and Y dim is %d\n", v1.x, v1.y);  
+  return 0;
+}
