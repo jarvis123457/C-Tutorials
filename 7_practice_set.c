279a895fb2f6df214ade11dea37af32ebd8f9631
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-09-01 19:35:39 +0500

    Update 7_practice_set.c

--- 7_practice_set.c
+++ 7_practice_set.c
@@ -1,1 +1,13 @@
-a
+/*                            PRACTICE SET                                      */
+/* Problem 1: Write a program to read three integers from a file.  */
+#include <stdio.h>
+
+int main(){
+  int a,b,c;
+  FILE *ptr;
+  ptr = fopen("7_practice_set_problem1.txt", "r");
+  fscanf(ptr, "%d %d %d", &a, &b, &c);
+  printf("The value of a, b and c is %d, %d, and %d respectively\n",a,b,c);
+  return 0;
+}
+/*--------------------------------------------------------------------------------------*/
