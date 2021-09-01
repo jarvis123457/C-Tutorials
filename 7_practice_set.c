372f4480a710772001af7a12798253d0ceefae02
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-09-01 20:16:01 +0500

    Update 7_practice_set.c

--- 7_practice_set.c
+++ 7_practice_set.c
@@ -28,7 +28,7 @@ for(int i=0;i<20;i++){
   return 0;
 }
 /*----------------------------------------------------------------------------------*/
-/* Problem 2: Write a program to read a text file character by character and write its content twice in a separate file  */
+/* Problem 3: Write a program to read a text file character by character and write its content twice in a separate file  */
 #include <stdio.h>
 
 int main(){
@@ -51,5 +51,30 @@ int main(){
   }
   fclose(ptr1);
   fclose(ptr2);
+  return 0;
+}
+/*--------------------------------------------------------------------------------*/
+/* Problem 4: Take name and salary of two employees as input from the user and write them to a text file in the following format
+  name1, 3300
+  name2, 7700  */
+#include <stdio.h>
+#include <string.h>
+int main(){
+  char c[20],d[20];
+  int a,b;
+  printf("Enter Name Of Employee 1\n");
+  gets(c);
+  printf("Enter Name Of Employee 2\n");
+  gets(d);
+  printf("Enter Salary Of Employee 1\n");
+  scanf("%d",&a);
+  printf("Enter Salary Of Employee 2\n");
+  scanf("%d",&b);
+  FILE *ptr;
+  ptr = fopen("7_practice_set_problem4.txt", "w");
+  fprintf(ptr, "%s has Salary: %d",c,a);
+  fputc('\n', ptr);
+  fprintf(ptr, "%s has Salary: %d",d,b);
+  fclose(ptr);
   return 0;
 }
