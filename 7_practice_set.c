581f027a6fd92e9070ebae1691ea6cf97905b0e5
jarvis123457 <62640976+jarvis123457@users.noreply.github.com>
2021-09-01 19:44:14 +0500

    Update 7_practice_set.c

--- 7_practice_set.c
+++ 7_practice_set.c
@@ -11,3 +11,20 @@ int main(){
   return 0;
 }
 /*--------------------------------------------------------------------------------------*/
+/* Problem 2: Write a program to generate multiplication table of a given nnumber in text format. Make sure that the file is readable and well-formatted.  */
+#include <stdio.h>
+
+int main(){
+  FILE *ptr;
+  ptr = fopen("7_practice_set_problem2.txt", "w");
+  printf("Enter the integer you need the table for: ");
+  int num;
+  scanf("%d",&num);
+for(int i=0;i<20;i++){
+  fprintf(ptr,"%d Multiplied By %d = %d\n",num,i+1,num*(i+1));
+}
+  fclose(ptr);
+  printf("Successfully generated table of %d\n",num);
+  return 0;
+}
+/*----------------------------------------------------------------------------------*/
